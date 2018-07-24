88ece96f2f15f0d647a4b7a36b27c2ab2d16293f
Alexander Brown <brownaa@lafayette.edu>
2018-07-23 21:50:57 -0400

    added master and axis arduino code

---
+++ arduino/axis/axis.ino
@@ -0,0 +1,243 @@
+#include <Wire.h>
+#include "i2cAnything.h"
+// Assign your channel in pins
+#define CHANNEL_A_PIN 0
+#define CHANNEL_B_PIN 1
+
+float testpos = 1.2345678;
+//for sending data back through i2c
+volatile byte* posFloatPtr;
+int Address = 3;
+
+
+boolean closedloop = true;
+
+float kp = 50.0;
+float ki = 0.0;
+float kd = 2.0;
+float command = 0;
+float oldcommand = 0;
+float oldoldcommand = 0;
+float battery_voltage = 4.0;
+
+float sinfreq = 2;
+float sinamp = PI/2;
+float sinangle = 0;
+
+unsigned long microsnow = 0;
+unsigned long oldmicros = 0;
+int dtmicros = 1000;
+float dt = .01;
+
+float e = 0;
+float dedt = 0;
+float inte = 0;
+float olde = 0;
+
+
+volatile long unCountShared = 0;
+
+
+int cpr = 64*20;
+
+float posrad = 0;
+float oldposrad = 0;
+float velrads = 0;
+
+int in1pin = 4;
+int in2pin = 5;
+int enpin = 6;
+
+int potval;
+float fV;
+int V;
+
+void setup() {
+  Wire.begin(Address);
+  Wire.onRequest(requestEvent);
+  Wire.onReceive(receiveEvent);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+delayMicroseconds(1000);
+  pinMode(enpin, OUTPUT);
+  delayMicroseconds(1000);
+  pinMode(in1pin, OUTPUT);
+  delayMicroseconds(1000);
+  pinMode(in2pin, OUTPUT);
+  //attach the interrupts
+  attachInterrupt(2, channelA, CHANGE);
+  attachInterrupt(3, channelB, CHANGE);
+
+}
+
+void loop() {
+
+  //encoder read
+  static long unCount;
+  noInterrupts();
+  unCount = unCountShared;
+  interrupts();
+
+  //timing
+  microsnow = micros();
+  dtmicros = microsnow - oldmicros;
+  dt = dtmicros / (1000000.0);
+  oldmicros = microsnow;
+
+  posrad = (unCount * 2.0 * PI) / (cpr * 1.0);
+  velrads = (posrad - oldposrad) / dt;
+
+
+  //read potentiometer
+  //int potval = analogRead(0);
+
+
+  //now compute the voltage command
+  if (closedloop) {
+    //figure out where we want motor to go
+    sinangle+=dt*sinfreq;
+//    float newcommand = sinamp*sin(sinangle)+(potval - 512.0) * 1.0 * PI / 512.0; //one full revolution
+    float newcommand = sinamp*sin(sinangle); //one full revolution
+
+    //filter the command position using a second order filter
+    //COMMAND IS NOW READ FROM i2c MASTER!!!
+    //command = newcommand;
+    
+    //now compute the error
+    e = command - posrad;
+    //error derivative
+    dedt = (e - olde) / dt;
+    //integral of error
+    inte = 0;//inte + e * dt;
+    //store old error value
+    olde = e;
+    //compute the voltage signal
+    fV = (kp * e + kd * dedt + ki * inte)*255.0/battery_voltage;
+    if(fV<-255){
+      fV=-255;
+    }
+    else if(fV>255){
+      fV=255;
+    }
+    //convert to counts (signed) -255 to 255 for analogWrite
+    V = int(fV);//fV * 255.0/battery_voltage;
+    
+  }
+
+
+  else {
+    //this is open loop then... so just map potentiometer position to V directly
+    fV = (potval - 512) * 255.0 / 512.0;
+    V = int(fV);
+
+  }
+
+
+
+
+  if (V < 0) {
+    digitalWrite(in1pin, LOW);
+    digitalWrite(in2pin, HIGH);
+    analogWrite(enpin, abs(V));
+  }
+  else {
+    digitalWrite(in1pin, HIGH);
+    digitalWrite(in2pin, LOW);
+    analogWrite(enpin, abs(V));
+  }
+
+  Serial.print(dt, 5);
+  Serial.print("\t");
+  Serial.print(V);
+  Serial.print("\t");
+  Serial.print(potval);
+  Serial.print("\t");
+  Serial.print(posrad, 5);
+
+  if(closedloop){
+  Serial.print("\t");
+  Serial.print(e, 3);
+  Serial.print("\t");
+  Serial.print(dedt, 3);
+  Serial.print("\t");
+  Serial.print(inte, 3);
+  }
+
+  Serial.println();
+
+  delayMicroseconds(1000);
+}
+
+
+  // function that executes whenever data is requested by master
+// this function is registered as an event, see setup()
+void requestEvent()
+{
+  //noInterrupts();
+  I2C_writeAnything(posrad);
+  I2C_writeAnything(command);
+  //interrupts();
+}
+void receiveEvent(int howMany){
+  if (howMany >= (sizeof command))
+   {
+    //noInterrupts();
+   I2C_readAnything (command);    
+    //interrupts();
+   }  // end if have enough data
+}
+
+// simple interrupt service routine
+void channelA()
+{
+  //Serial.println("A");
+  if (digitalRead(CHANNEL_A_PIN) == HIGH)
+  {
+    if (digitalRead(CHANNEL_B_PIN) == LOW)
+    {
+      unCountShared++;
+    }
+    else
+    {
+      unCountShared--;
+    }
+  }
+  else
+  {
+    if (digitalRead(CHANNEL_B_PIN) == HIGH)
+    {
+      unCountShared++;
+    }
+    else
+    {
+      unCountShared--;
+    }
+  }
+}
+
+void channelB()
+{
+  //Serial.println("B");
+  if (digitalRead(CHANNEL_B_PIN) == HIGH)
+  {
+    if (digitalRead(CHANNEL_A_PIN) == HIGH)
+    {
+      unCountShared++;
+    }
+    else
+    {
+      unCountShared--;
+    }
+  }
+  else
+  {
+    if (digitalRead(CHANNEL_A_PIN) == LOW)
+    {
+      unCountShared++;
+    }
+    else
+    {
+      unCountShared--;
+    }
+  }
+}
---
+++ arduino/master/master.ino
@@ -0,0 +1,77 @@
+#include <Wire.h>
+#include "i2cAnything.h"
+
+//float feedback;
+//float voltage;
+float cmd1 = 0;
+float cmd2 = 0;
+float cmd3 = 0;
+float cmd4 = 0;
+float cmd5 = 0;
+
+float command1_fdbk = 0;
+float command2_fdbk = 0;
+
+float feedback1 =0;
+float feedback2 = 0;
+float feedback3 = 0;
+float feedback4 = 0;
+
+int address1 = 2;
+int address2 = 3;
+int address3 = 4;
+int address4 = 5;
+int address5 = 6;
+
+void setup(){
+  Serial.begin(115200);
+  Wire.begin();
+  
+}
+
+void loop()
+{
+  //first, read the command(s) from the serial monitor
+if (Serial.available())
+{
+ char myChar = Serial.read();
+ if (myChar == '!')
+ {
+   cmd1 = Serial.parseFloat();
+   cmd2 = Serial.parseFloat();
+   //now send feedback about what we saw
+//   Serial.print(cmd1);
+//   Serial.print("\t");
+//   Serial.print(command1_fdbk);
+//   Serial.print("\t");
+   Serial.print(feedback1);
+   Serial.print("\t");
+//   Serial.print(cmd2);
+//   Serial.print("\t");
+//   Serial.print(command2_fdbk);
+//   Serial.print("\t");
+   Serial.println(feedback2);
+ }
+}
+  //then, send the command to the servo board
+  Wire.beginTransmission (address1);
+  I2C_writeAnything (cmd1);
+  Wire.endTransmission ();
+  //now, receive feedback from the servo board.
+   Wire.beginTransmission(address1);
+   Wire.requestFrom(address1, sizeof(feedback1)+sizeof(cmd1));              // request 8 bytes from slave device #2
+   I2C_readAnything(feedback1);
+   I2C_readAnything(command1_fdbk);
+   Wire.endTransmission();
+   Wire.beginTransmission (address2);
+    I2C_writeAnything (cmd2);
+    Wire.endTransmission ();
+   Wire.beginTransmission(address2);
+   Wire.requestFrom(address2, sizeof(feedback2)+sizeof(cmd2));              // request 8 bytes from slave device #2
+   I2C_readAnything(feedback2);
+   I2C_readAnything(command2_fdbk);
+   Wire.endTransmission();
+   
+   delay(1);
+   
+}
