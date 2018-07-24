20fbe43c071043ea9c919ac2b7e7c89e9ec85420
brownaa <brownaa@lafayette.edu>
2018-07-24 09:54:13 -0400

    communication with gantry and ROS works. Two axes only for now.

--- arduino/master/master.ino
+++ arduino/master/master.ino
@@ -8,6 +8,7 @@ float cmd2 = 0;
 float cmd3 = 0;
 float cmd4 = 0;
 float cmd5 = 0;
+float tailcommand = 0;
 
 float command1_fdbk = 0;
 float command2_fdbk = 0;
@@ -16,6 +17,7 @@ float feedback1 =0;
 float feedback2 = 0;
 float feedback3 = 0;
 float feedback4 = 0;
+float feedback5 = 0;
 
 int address1 = 2;
 int address2 = 3;
@@ -39,6 +41,10 @@ if (Serial.available())
  {
    cmd1 = Serial.parseFloat();
    cmd2 = Serial.parseFloat();
+   cmd3 = Serial.parseFloat();
+   cmd4 = Serial.parseFloat();
+   cmd5 = Serial.parseFloat();
+   tailcommand = Serial.parseFloat();
    //now send feedback about what we saw
 //   Serial.print(cmd1);
 //   Serial.print("\t");
@@ -50,7 +56,14 @@ if (Serial.available())
 //   Serial.print("\t");
 //   Serial.print(command2_fdbk);
 //   Serial.print("\t");
-   Serial.println(feedback2);
+   Serial.print(feedback2);
+   Serial.print("\t");
+   Serial.print(feedback3);
+   Serial.print("\t");
+   Serial.print(feedback4);
+   Serial.print("\t");
+   Serial.print(feedback5);
+   Serial.println();
  }
 }
   //then, send the command to the servo board
--- nodes/gantry_node.py
+++ nodes/gantry_node.py
@@ -9,63 +9,70 @@ import sys
 import datetime
 import rospkg
 import nav_msgs
+from geometry_msgs.msg import PoseStamped
 
 class FishGantry():
+  def __init__(self):
+    self.port = rospy.get_param('~port','/dev/ttyUSB0')
+    self.baud = rospy.get_param('~baud',115200)
+    self.ser = serial.Serial(self.port, self.baud,timeout=1) #this initializes the serial object
+    self.pub= rospy.Publisher("fishgantry/robotpose",PoseStamped)
+    self.goalpose_sub = rospy.Subscriber("/gantry/commandpose",PoseStamped,self.commandCallback)
+    
+    #initialize a command position
+    self.command = PoseStamped()
+    self.command.header.stamp = rospy.Time.now()
+    self.command.pose.position.x = 0
+    self.command.pose.position.y = 0
+    self.command.pose.position.z = 0
 
+    #main loop runs on a timer, which ensures that we get timely updates from the gantry arduino
+    rospy.Timer(rospy.Duration(.01),self.loop,oneshot=False) #timer callback (math) allows filter to run at constant time
+    #subscribers (inputs)
+    #construct the file name for our text output file
+    rospack = rospkg.RosPack()
+    # get the file path for rospy_tutorials
+    self.package_path=rospack.get_path('fishgantry_ros')
 
-   def __init__(self):
-      self.port = rospy.get_param('~port','/dev/ttyUSB0')
-      self.baud = rospy.get_param('~baud',115200)
-      self.ser = serial.Serial(self.port, self.baud,timeout=1) #this initializes the serial object
-      self.pub= rospy.Publisher("fishgantry/robotpose",PoseStamped)
-      self.goalpose_sub = rospy.Subscriber("/gantry/commandpose",PoseStamped,self.commandCallback)
+  def commandCallback(self,data):
+    self.command.header.stamp = data.header.stamp
+    self.command.pose.position.x = data.pose.position.x
+    self.command.pose.position.y = data.pose.position.y
+    #print "received: "+str(self.command.pose.position.x)
+
+  def loop(self,event):
+    serstring = '!'+str(self.command.pose.position.x)+','+str(self.command.pose.position.y)+','+str(0)+','+str(0)+','+str(0)+','+str(0)+'\r\n'
+    print "sending: "+serstring
+    self.ser.write(serstring)
+    line = self.ser.readline()
+    print line
+    linestrip = line.strip('\r\n')
+    linesplit = line.split()
+    if len(linesplit)>=3:
+      #print shotslast, arduinonumshots,shotslast==(arduinonumshots+1)
       
-      #initialize a command position
-      self.command = PoseStamped()
-      self.command.header.stamp = rospy.Time.now()
-      self.pose.position.x = 0
-      self.pose.position.y = 0
-      self.pose.position.z = 0
+      try:
 
-      #main loop runs on a timer, which ensures that we get timely updates from the gantry arduino
-      rospy.Timer(rospy.Duration(.005),self.loop,oneshot=False) #timer callback (math) allows filter to run at constant time
-      #subscribers (inputs)
-      #construct the file name for our text output file
-      rospack = rospkg.RosPack()
-      # get the file path for rospy_tutorials
-      self.package_path=rospack.get_path('fishgantry_ros')
-
-   def loop(self,event):
-      line = self.ser.readline()
-      print line
-      linestrip = line.strip('\r\n')
-      linesplit = line.split()
-      if len(linesplit)>=3:
-        #print shotslast, arduinonumshots,shotslast==(arduinonumshots+1)
-        
-        try:
-
-            msg = TetherFishMsg()
-            msg.header.stamp = rospy.Time.now()
-            msg.robotpos = float(linesplit[0])
-            msg.robotangle = float(linesplit[1])
-            msg.arduino_time = float(linesplit[2])
-            #msg.tailfreq= float(linesplit[3])
-            #msg.tailamp = float(linesplit[4])
-            #msg.tailangle = float(linesplit[5])
-            print "publishing"
-            self.pub.publish(msg)
-            
-
-        except:
-            print "OOPS! BAD LINE"
-            #ef.write("problem  with serial line"+"\r\n")
+        msg = PoseStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.pose.position.x = float(linesplit[0])
+        msg.pose.position.y = float(linesplit[1])
+        #msg.arduino_time = float(linesplit[2])
+        #msg.tailfreq= float(linesplit[3])
+        #msg.tailamp = float(linesplit[4])
+        #msg.tailangle = float(linesplit[5])
+        #print "publishing"
+        self.pub.publish(msg)
+              
+      except:
+        print "OOPS! BAD LINE"
+        #ef.write("problem  with serial line"+"\r\n")
 
       
 #main function
 def main(args):
   rospy.init_node('gantry_node', anonymous=True)
-  encreader = ArduinoTarget()
+  encreader = FishGantry()
   
   try:
     rospy.spin()
